869b1c0b998ec4717dc0026f1af0a4b6a1e526d1
Jim Finn <jim.finnjim2001@gmail.com>
2025-04-02 08:15:41 +0000

    Added Button A

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
 serial.redirectToUSB()
 basic.forever(function () {
     serial.writeValue("Light", input.lightLevel())
